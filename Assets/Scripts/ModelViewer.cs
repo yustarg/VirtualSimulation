7635986a0d0946c472de2b76162174b9838661e7
Alan <yustar130@gmail.com>
2022-06-03 19:49:44 -0700

    [add] ModelViewer basic move and rotate

--- Assets/Scripts/ModelViewer.cs
+++ Assets/Scripts/ModelViewer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,16 @@ namespace BCIT
 {
     public class ModelViewer : MonoBehaviour
     {
+        private readonly float rotateSpeeed = 5f;
+        private readonly float moveSpeeed = 5f;
+        
+        
         // Start is called before the first frame update
         void Start()
         {
             InputManager.Instance.OnRightButtonDown += Rotate;
+            InputManager.Instance.OnMiddleButtonDown += Move;
+            InputManager.Instance.OnLeftButtonDown += MoveSinglePart;
         }
 
         // Update is called once per frame
@@ -18,9 +25,26 @@ namespace BCIT
             
         }
 
+        private void OnDestroy()
+        {
+            InputManager.Instance.OnRightButtonDown -= Rotate;
+            InputManager.Instance.OnMiddleButtonDown -= Move;
+            InputManager.Instance.OnLeftButtonDown -= MoveSinglePart;
+        }
+
+        private void Move(float x, float y)
+        {
+            transform.Translate(x, y, 0, Space.World);
+        }
+        
         private void Rotate(float x, float y)
         {
-            Debug.Log(x + " " + y);
+            transform.Rotate(new Vector3(y, x, 0f) * rotateSpeeed);
+        }
+
+        private void MoveSinglePart(float x, float y)
+        {
+            
         }
     }
 }
